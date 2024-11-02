0ec8a7088851cc77136d226bd4fbc290a9ce25d1
alexandrefnas <alexandrefnas@gmail.com>
2024-11-02 01:28:18 -0300

    first commit

---
+++ src/Components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height:75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-family: 'Roboto';    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+       
+        text-align: right;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF
+    }
+`
+
